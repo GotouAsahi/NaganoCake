31e5fd5954c6c4f0f094d24b643b228850bc336d
EC2 Default User <ec2-user@ip-172-31-18-90.ap-northeast-1.compute.internal>
2023-04-14 03:48:28 +0000

    1_認証機能完成

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,26 @@ Rails.application.routes.draw do
     sessions: "public/sessions"
   }
 
+  scope module: :public do
+    root to: "homes#top"
+    get '/about' => "homes#about", as: "about"
+    get 'customers/mypage' => "customers#show", as: "mypage"
+    get 'customers/edit'
+    get 'customers/update'
+    get 'customers/exit'
+    get 'customers/unsbscribe'
+  end
+
+  namespace :public do
+  end
+
   # 管理者用
   devise_for :admin,skip: [:registrations,:passwords], controllers: {
     sessions: "admin/sessions"
   }
+
+  namespace :admin do
+    root to: "homes#top"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
