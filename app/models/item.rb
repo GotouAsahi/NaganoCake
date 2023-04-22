2c4ba70b4d08b9ef368e4be3f8874b14565cede0
EC2 Default User <ec2-user@ip-172-31-18-90.ap-northeast-1.compute.internal>
2023-04-22 10:57:42 +0000

    3_商品登録完成

---
+++ app/models/item.rb
@@ -0,0 +1,18 @@
+class Item < ApplicationRecord
+  belongs_to :genre
+
+  has_one_attached :image
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :notax_price, presence: true,numericality: {only_integer: true}
+  validates :genre_id, presence: true
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+end
