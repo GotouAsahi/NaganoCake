9378c89ca16eca3adaf1a0799279c82e9f57aea8
EC2 Default User <ec2-user@ip-172-31-18-90.ap-northeast-1.compute.internal>
2023-05-10 04:43:11 +0000

    Add genre-based product search

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,18 @@
 class Public::ItemsController < Public::BaseController
+  before_action :authenticate_customer!, except: [:index,:show]
   def index
-    @items = Item.all
+    if params[:genre_id].nil?
+      @items = Item.where(is_active: true)
+    else
+      @items = Item.where(is_active: true).where(genre_id: params[:genre_id])
+    end
+
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
 end
